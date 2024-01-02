1450323e4aea4d06033ff873ffca6e69c357dcdc
Juqutsu <84779567+Juqutsu@users.noreply.github.com>
2024-01-02 09:27:42 +0000

    update 2

--- app.js
+++ app.js
@@ -26,4 +26,4 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:350, origin:'left'})
 sr.reveal('.home-img',{delay:350, origin:'right'})
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.sub-service,.about',{delay:200, origin:'bottom'})
