ada0e3eae1edcaceb52d931c132fc09ede4fc37e
Juqutsu <84779567+Juqutsu@users.noreply.github.com>
2024-01-02 10:05:15 +0000

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/personal',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f95b4922d013cb381fcc8e2994463dbc5d96e581',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/personal' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f95b4922d013cb381fcc8e2994463dbc5d96e581',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
